dbb1e9c928ae3d5340866af02fd0f685d46969a6
JarandDev <jarand95@gmail.com>
2022-12-13 21:51:46 +0100

    #1: Account registration

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,16 +1,26 @@
 import {Box, Button, Container} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 function Home() {
+    const navigate = useNavigate();
+
+    const navigateToLogin = () => {
+        navigate("/login");
+    };
+
+    const navigateToRegister = () => {
+        navigate("/register");
+    };
 
     return (
         <Container maxWidth="sm">
             <h3>Welcome to OAuth Administration</h3>
-            <Box sx={{'& .MuiButton-root': {m: 1}}}>
+            <Box sx={{"& .MuiButton-root": {m: 1}}}>
                 <div>
-                    <Button id="login" type="button" variant="contained" size="large" fullWidth href="/login">Login</Button>
+                    <Button id="login" type="button" variant="contained" size="large" fullWidth onClick={navigateToLogin}>Login</Button>
                 </div>
                 <div>
-                    <Button id="register" type="button" variant="contained" size="large" fullWidth href="/register">Register</Button>
+                    <Button id="register" type="button" variant="contained" size="large" fullWidth onClick={navigateToRegister}>Register</Button>
                 </div>
             </Box>
         </Container>
